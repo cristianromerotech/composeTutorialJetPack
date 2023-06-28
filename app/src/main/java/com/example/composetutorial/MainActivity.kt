b4153fe8eb8a11c20eb6f884df40103f301152b6
cristianfernandoromero1 <cfomero99@gmail.com>
2023-06-28 14:46:08 -0500

    update with button

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.composetutorial
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+
 import androidx.compose.foundation.layout.padding
 
 /*class MainActivity : AppCompatActivity() {
@@ -15,11 +17,11 @@ import androidx.compose.foundation.layout.padding
 }*/
 
 import androidx.compose.material.Text
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -27,26 +29,43 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-
-                ) {
-                    Greeting("Android")
-                }
+            MyApp()
             }
         }
 
+@Composable
+    fun MyApp(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose")
+    ) {
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
+    }
+    }
 
     @Composable
     private fun Greeting(name: String) {
-        //Text(text = "Hello $name!")
+        Surface(
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+        )
         //change color text to green
         //Text(text = "Hello $name!", color = Color.Green)
-
-        Text(text = "Hello $name!", modifier = Modifier.padding(34.dp))
+        {
+            Row(modifier = Modifier.padding(24.dp)) {
+                Column(modifier = Modifier.weight(1f)) {
+                    Text(text = "Hello, ")
+                    Text(text = name)
+                }
+                ElevatedButton(
+                    onClick = { /* TODO */ }
+                ) {
+                    Text("Show more")
+                }
+            }
+    }
 
     }
 
