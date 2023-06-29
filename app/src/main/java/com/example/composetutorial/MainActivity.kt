92781cf07bfbedcee4cae1d2b22e791c48783ae0
cristianfernandoromero1 <cfomero99@gmail.com>
2023-06-29 18:50:35 -0500

    comment

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -9,23 +9,19 @@ import androidx.compose.foundation.layout.Row
 
 import androidx.compose.foundation.layout.padding
 
-/*class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}*/
 
 import androidx.compose.material.Text
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity() { //app
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -47,26 +43,30 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     private fun Greeting(name: String) {
+
+        val expanded = remember { mutableStateOf(false) }
+
+        val extraPadding = if (expanded.value) 48.dp else 0.dp
+
         Surface(
             color = MaterialTheme.colorScheme.primary,
             modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
-        )
-        //change color text to green
-        //Text(text = "Hello $name!", color = Color.Green)
-        {
+        ) {
             Row(modifier = Modifier.padding(24.dp)) {
-                Column(modifier = Modifier.weight(1f)) {
+                Column(modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+                ) {
                     Text(text = "Hello, ")
                     Text(text = name)
                 }
                 ElevatedButton(
-                    onClick = { /* TODO */ }
+                    onClick = { expanded.value = !expanded.value }
                 ) {
-                    Text("Show more")
+                    Text(if (expanded.value) "Show less" else "Show more")
                 }
             }
-    }
-
+        }
     }
 
     @Preview(showBackground = true, name = "Text preview")
